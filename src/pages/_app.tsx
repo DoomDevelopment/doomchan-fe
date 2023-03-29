da22cef2f4dd5978764e6d0f94d7bcd11e0d579c
IlijaRad <ilija.radivojevic237@gmail.com>
2023-03-30 01:57:19 +0200

    First commit

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,15 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import { Inter } from "next/font/google";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
+const inter = Inter({ subsets: ["latin"] });
+
+const App = ({ Component, pageProps }: AppProps) => {
+  return (
+    <div className={inter.className}>
+      <Component {...pageProps} />
+    </div>
+  );
+};
+
+export default App;
